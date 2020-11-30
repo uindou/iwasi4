12d78a1c7e077fe9a4df91605d85674d3f4bea00
kota <watako0126w@gmail.com>
2020-11-30 23:39:58 +0900

    ジャンプアニメーション修正

---
+++ Assets/JumpManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class JumpManager : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)){
+            animator.SetBool("jump",true);
+        }
+    }
+}
