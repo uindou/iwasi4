05fe117013be9409f39728725264f23193232692
toya akiyama <akym.tohoku@gmail.com>
2020-12-21 22:58:23 +0900

    左右に床を設置

--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -6,27 +6,35 @@ public class StageManager : MonoBehaviour
 {
     private int stageNum;
     public GameObject Stage;
+    public GameObject subStage;
     public Transform Player;
     public float deletePoint;
 
-    private Queue<GameObject> q;
+    private Queue<(GameObject, GameObject, GameObject)> q;
     private Transform OldStage;
     private int currentZ;
 
     void Awake()
     {
-        q = new Queue<GameObject>();
+        q = new Queue<(GameObject,GameObject,GameObject)>();
         stageNum = 5;
         currentZ = - 1;
         for (int i = 0; i < stageNum; i++)
         {
             currentZ += 1;
             Vector3 zahyo = new Vector3(0, 0, i * 50);
+            Vector3 zahyoR = new Vector3(50, 0, i * 50);
+            Vector3 zahyoL = new Vector3(-50, 0, i * 50);
             GameObject obj = Instantiate(Stage, zahyo, Quaternion.identity);
-            q.Enqueue(obj);
-            StageInit(obj);
+            GameObject objR = Instantiate(subStage, zahyoR, Quaternion.identity);
+            GameObject objL = Instantiate(subStage, zahyoL, Quaternion.identity);
+            var objs = (obj, objR, objL);
+            q.Enqueue(objs);
+            StageInit(objs);
         }
-        OldStage = q.Peek().transform;
+
+        var (obj2, obj2R, obj2L) = q.Peek();
+        OldStage = obj2.transform;
     }
 
     // Start is called before the first frame update
@@ -47,18 +55,25 @@ public class StageManager : MonoBehaviour
             StageDelete(q.Dequeue());
             StagePush();
         }
-        OldStage = q.Peek().transform;
+        var (obj, objR, objL) = q.Peek();
+        OldStage = obj.transform;
     }
     void StagePush()
-    {
+    { 
         Vector3 zahyo = new Vector3(0, 0, currentZ * 50);
+        Vector3 zahyoR = new Vector3(50, 0, currentZ * 50);
+        Vector3 zahyoL = new Vector3(-50, 0, currentZ * 50);
         GameObject obj = Instantiate(Stage, zahyo, Quaternion.identity);
-        q.Enqueue(obj);
-        StageInit(obj);
+        GameObject objR = Instantiate(subStage, zahyoR, Quaternion.identity);
+        GameObject objL = Instantiate(subStage, zahyoL, Quaternion.identity);
+        var objs = (obj, objR, objL);
+        q.Enqueue(objs);
+        StageInit(objs);
         currentZ += 1;
     }
-    void StageInit(GameObject obj)//ステージの付属パーツ(石や木)を作成して、ステージの子供にする。(ステージ自体の作成はStagePushで)
+    void StageInit((GameObject, GameObject, GameObject) objs)//ステージの付属パーツ(石や木)を作成して、ステージの子供にする。(ステージ自体の作成はStagePushで)
     {
+        var (obj, objR, objL) = objs;
         GameObject target = obj.transform.GetChild(0).gameObject;
         //石を追加する処理
         partsPutter rock;
@@ -66,13 +81,16 @@ public class StageManager : MonoBehaviour
         rock.Init((float)currentZ*50,obj.transform);
     }
 
-    void StageDelete(GameObject obj)//ステージとその子供を削除する。
+    void StageDelete((GameObject, GameObject, GameObject) objs)//ステージとその子供を削除する。
     {
+        var (obj, objR, objL) = objs;
         foreach (Transform child in obj.transform)
         {
             // 一つずつ破棄する
             Destroy(child.gameObject);
         }
         Destroy(obj);
+        Destroy(objR);
+        Destroy(objL);
     }
 }
