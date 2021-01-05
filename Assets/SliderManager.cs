e5ca6b48bb515bac0e738ca1cf7878d8f41362dc
kota <watako0126w@gmail.com>
2021-01-05 22:22:51 +0900

    slidermanager作成

---
+++ Assets/SliderManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    private float stageLength;
+    public Slider
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stageLength = Database.StageLength()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
