ea774a158bb6ee59eaaa4aaedec3a27e88a0ccfc
toya akiyama <akym.tohoku@gmail.com>
2021-01-05 22:34:07 +0900

    進捗バー

--- Assets/SliderManager.cs
+++ Assets/SliderManager.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 public class SliderManager : MonoBehaviour
 {
     private float stageLength;
-    public Slider
+    public Transform player;
+    public Slider progress;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        stageLength = Database.StageLength()
+        stageLength = DataBase.StageLength();
+        progress.maxValue = stageLength;
+        progress.minValue = 25;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        progress.value = player.position.z;
     }
 }
