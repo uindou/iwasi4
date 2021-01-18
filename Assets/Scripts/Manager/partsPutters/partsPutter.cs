7d84d6177339ab544299eef67b97cf8ca6b11b8a
toya akiyama <akym.tohoku@gmail.com>
2021-01-18 21:58:35 +0900

    フォルダ分け、partsPutterを使いやすくした

---
+++ Assets/Scripts/Manager/partsPutters/partsPutter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class partsPutter : MonoBehaviour
+{
+
+
+    //ステージ生成時に呼び出されて、与えられたオブジェクトの子供になる石,木を配置する。削除はstage削除の責任
+    public virtual void Init(Transform obj,bool isCenter)
+    {
+        
+    }
+
+    public virtual void makeStones(Transform obj)
+    {
+        
+    }
+    
+    //地上の物体を生成するやつ。setObjがオブジェクト、setNumが設置する個数。
+    public virtual void makeGroundParts( Transform obj,GameObject setObj,int setNum)
+    {
+        
+    }
+
+}
